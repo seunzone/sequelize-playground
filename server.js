e4ef873d1c577678affd7de5bdee9a4c3eec9dca
seun <darealseun@gmail.com>
2018-02-23 11:48:23 +0100

    established basic connection

---
+++ server.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('seqcal', 'postgres', null, {
+  host: 'localhost',
+  dialect: 'postgres',
+
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
