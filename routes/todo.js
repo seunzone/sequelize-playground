b702b2e1524dea8d4903a11438b8696e9cf2f224
seun <darealseun@gmail.com>
2018-03-09 16:59:28 +0100

    Add logic to using async-await

--- routes/todo.js
+++ routes/todo.js
@@ -2,84 +2,86 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models/index');
 
+//middleware to hanlde errors
+const awaitErorrHandlerFactory = middleware => {
+  return async (req, res, next) => {
+    try {
+      await middleware(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
 /* GET todo listing. */
-router.get('/', function(req, res, next) {
-  model.Todo.findAll({})
-        .then(todos => res.json({
-            error: false,
-            data: todos
-        }))
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
-});
+router.get(
+  "/",
+  awaitErorrHandlerFactory(async (req, res, next) => {
+    const todos = await model.Todo.findAll({});
+    return res.json({
+      error: false,
+      data: todos
+    });
+  })
+);
 
 
 /* POST todo. */
-router.post('/', function(req, res, next) {
-  const {
-         title,
-         description
-     } = req.body;
-     model.Todo.create({
-             title: title,
-             description: description
-         })
-         .then(todo => res.status(201).json({
-             error: false,
-             data: todo,
-             message: 'New todo has been created.'
-         }))
-         .catch(error => res.json({
-             error: true,
-             data: [],
-             error: error
-         }));
-});
+router.post(
+  "/",
+  awaitErorrHandlerFactory(async (req, res, next) => {
+    const { title, description } = req.body;
+    const todo = model.Todo.create({
+      title: title,
+      description: description
+    });
+    return res.status(201).json({
+      error: false,
+      data: todo,
+      message: "New todo has been created."
+    });
+  })
+);
 
 
 /* update todo. */
-router.put('/:id', function(req, res, next) {
-  const todo_id = req.params.id;
+router.put(
+  "/:id",
+  awaitErorrHandlerFactory(async (req, res, next) => {
+    const todo_id = req.params.id;
 
-      const { title, description } = req.body;
+    const { title, description } = req.body;
 
-      model.Todo.update({
-              title: title,
-              description: description
-          }, {
-              where: {
-                  id: todo_id
-              }
-          })
-          .then(todo => res.status(201).json({
-              error: false,
-              message: 'todo has been updated.'
-          }))
-          .catch(error => res.json({
-              error: true,
-              error: error
-          }));
-});
+    model.Todo.update(
+      { title: title, description: description },
+      { where: { id: todo_id } }
+    );
+
+    return res.status(201).json({
+      error: false,
+      message: "todo has been updated."
+    });
+  })
+);
 
 
-/* DELETE todo listing. */
-router.delete('/:id', function(req, res, next) {
-  const todo_id = req.params.id;
+/* Delete todo. */
+router.delete(
+  "/:id",
+  awaitErorrHandlerFactory(async (req, res, next) => {
+    const todo_id = req.params.id;
 
-      model.Todo.destroy({ where: {
-          id: todo_id
-      }})
-          .then(status => res.status(201).json({
-              error: false,
-              message: 'todo has been delete.'
-          }))
-          .catch(error => res.json({
-              error: true,
-              error: error
-          }));
-});
+    model.Todo.destroy({
+      where: {
+        id: todo_id
+      }
+    });
+
+    return res.status(201).json({
+      error: false,
+      message: "todo has been delete."
+    });
+  })
+);
 
 module.exports = router;
